8cf633739cca2d1a3f9ea6ddac51593864d38951
Michal Kubele <michal.kubele@avast.com>
2021-12-11 00:40:33 +0100

    Add Day1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,54 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val input = readInput("Day01").map { it.toInt() }
+    val resultMap = mutableMapOf<Int, Result>()
+    val resultList = mutableListOf<Int>()
+
+    fun compareValuesResult(index: Int, currentValue: Int, previousValue: Int) {
+        when {
+            currentValue > previousValue -> resultMap[index] = Result.INCREASE
+            currentValue < previousValue -> resultMap[index] = Result.DECREASE
+            currentValue == previousValue -> resultMap[index] = Result.NO_CHANGE
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun getResults(input: List<Int>) {
+        for (i in input.indices) {
+            try {
+                compareValuesResult(i, input[i], input[i - 1])
+            } catch (e: IndexOutOfBoundsException) {
+                resultMap[i] = Result.NA
+            }
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part1(input: List<Int>): Int {
+        getResults(input)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+        return resultMap.filter { it.value == Result.INCREASE }.size
+    }
+
+    fun getSum(index: Int) = input[index] + input[index + 1] + input[index + 2]
+
+    fun part2(input: List<Int>): Int {
+        for (i in input.indices) {
+            try {
+                resultList.add(getSum(i))
+            } catch (_: IndexOutOfBoundsException) {
+            }
+        }
+
+        getResults(resultList)
+
+        return resultMap.filter { it.value == Result.INCREASE }.size
+    }
+
+//    println(part1(input))
+//    println(part2(input))
+}
+
+enum class Result {
+    NA,
+    INCREASE,
+    DECREASE,
+    NO_CHANGE
 }
